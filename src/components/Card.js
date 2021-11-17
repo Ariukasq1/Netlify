1ee90817b961daac0f77050ef419117a811d397b
anu-ujin <anu.b@nmma.co>
2021-11-17 22:13:21 +0800

    fix some responsive

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,20 @@
 import React from "react";
-import Link from 'next/link'
 
 const Card = (props) => {
-    return (
-        <Link href={props.linkTo}>
-          <div data-content={ props.content && props.content.substring(0, 60) + "..."} data-color={" " }className="item card" >
-            <div className="card-text">
-              {props.text}
-            </div>
-            <div className="img-container">
-             {props.iconsrc && <img src={props.iconsrc} />} 
-            </div>
-          </div>
-        </Link>
-    );
+  return (
+    <a href={props.linkTo}>
+      <div
+        data-content={props.content && props.content.substring(0, 60) + "..."}
+        data-color={" "}
+        className="item card"
+      >
+        <div className="card-text">{props.text}</div>
+        <div className="img-container">
+          {props.iconsrc && <img src={props.iconsrc} />}
+        </div>
+      </div>
+    </a>
+  );
 };
 
-export default Card
+export default Card;
